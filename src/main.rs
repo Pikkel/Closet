615ced2fcd9836fbe970db2a953cd9ef543f2209
Pikkel <76259034+Pikkel@users.noreply.github.com>
2021-12-03 08:49:35 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[tokio::main]
 async fn main() {
-    warp::serve(warp::fs::file("index.html"))
-        .run(([0, 0, 0, 0], 700))
+    warp::serve(warp::fs::dir("Private"))
+    warp::serve(warp::fs::dir("Public"))
+        .run(([0, 0, 0, 0], 1337))
         .await;
 }
